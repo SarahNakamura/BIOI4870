c2aac05b96779bf8dafa400685966dcea07c01f0
SarahNakamura <snakamura@unomaha.edu>
2024-04-22 20:42:56 -0500

    update piechart.php
    remove piechart.html
    (html can be implemented in php file)

--- html/piechart.php
+++ html/piechart.php
@@ -1,114 +1,74 @@
 <?php
-   $servername = "localhost";
-   $username = "snakamura";
-   $password = "";
-   $dbname = "snakamura";
+$servername = "localhost";
+$username = "username";
+$password = "";
+$dbname = "database_name";
 
-   // connect the database with the server
-   $conn = new mysqli($servername,$username,$password,$dbname);
+// connect the database with the server
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    // if error occurs
-    if ($conn -> connect_errno)
-    {
-       echo "Failed to connect to MySQL: " . $conn -> connect_error;
-       exit();
+// if error occurs
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
+    exit();
+}
+
+$sql = "SELECT * FROM COL10A1_mutation";
+$result = $conn->query($sql);
+
+//declare array to store the data of database
+$row = [];
+
+if ($result->num_rows > 0) {
+    // fetch all data from db into array
+    $row = $result->fetch_all(MYSQLI_ASSOC);
+}
+
+$dataArray = [];
+if ($row) {
+    foreach ($row as $r) {
+        $dataArray[] = array($r['name'], $r['frequency']);
     }
-
-    $sql = "SELECT * FROM COL10A1_mutation";
-    $result = ($conn->query($sql));
-    //declare array to store the data of database
-    $row = [];
-
-    if ($result->num_rows > 0)
-    {
-        // fetch all data from db into array
-        $row = $result->fetch_all(MYSQLI_ASSOC);
-    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <html>
-  <head>
+<head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
+        function drawChart() {
 
-        var data = google.visualization.arrayToDataTable([
-          ['Mutation', 'Frequency']
-          <?php
-                while($chart = mysqli_fetch_assoc($result))
-                echo "['$chart['name']','$chart['frequency']']"
-          ?>
-        ]);
-
-        var options = {
-          title: 'Frequency of Mutation by Mutation Type'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-      }
-    </script>
-  </head>
-  <body>
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
-  </body>
-</html>
-
-<?php
-    mysqli_close($conn);
-?>
-
-
-
-
-
-
-
-
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gene Mutation Pie Chart</title>
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Mutation', 'Mutation Frequency'],
-
-          <?php
-
-                while($chart = mysqli_fetch_assoc($result))
-                {
-                    echo "['".$chart['name']."','".$chart['frequency']."']"
+            var data = google.visualization.arrayToDataTable([
+                ['Mutation', 'Frequency'],
+                <?php
+                if ($dataArray) {
+                    foreach ($dataArray as $da) {
+                        echo "['" . $da[0] . "', " . $da[1] . "],";
+                    }
                 }
-          ?>
+                ?>
+            ]);
 
-        ]);
+            var options = {
+                title: 'Frequency of Mutation by Mutation Type'
+            };
 
-        var options = {
-          title: 'My Daily Activities'
-        };
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-      }
+            chart.draw(data, options);
+        }
     </script>
 </head>
 <body>
-  <div id="piechart" style="width: 900px; height: 500px;"></div>
+<div id="piechart" style="width: 900px; height: 500px;"></div>
 </body>
 </html>
+
+<?php
+$conn->close();
+?>
