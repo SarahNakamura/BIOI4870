f46c163b2665dc92e64f678a46f5c843b7f483c3
SarahNakamura <snakamura@unomaha.edu>
2024-04-23 21:12:38 -0500

    pie chart

--- pie_chart/BRCA/COL10A1.php
+++ pie_chart/BRCA/COL10A1.php
@@ -36,7 +36,7 @@ if ($row) {
 <html lang="en">
 <html>
 <head>
-    <h1 style=background-color:#FDFF8B>Gene:TFF1</h1>
+    <h1 style=background-color:#FDFF8B>Gene:COL10A1</h1>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
         google.charts.load('current', {'packages':['corechart']});
